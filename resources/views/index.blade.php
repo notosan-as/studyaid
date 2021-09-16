c4bec63b405b6e9b22d28a5b730f92fa18fcc706
のとさん <yuumi.umi56@gmail.com>
2021-09-16 23:52:42 +0900

    indexに画像を追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,4 +4,15 @@
 
 @section('content')
 @include('nav')
-index
+<!-- Background image -->
+<div class="p-5 text-center bg-image"
+    style="background-image: url('image/index_image.webp');height: 400px;">
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-body">
+            <h1 class="mb-3">Studyaid</h1>
+            <h4 class="mb-3">勉強を楽しく記録しよう</h4>
+        </div>
+    </div>
+</div>
+<!-- Background image -->
+studyaidとは
