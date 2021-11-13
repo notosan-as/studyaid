186fad344e5b571cf73bb8bfaf2d0e6e4f895081
notosan-as <yuumi.umi56@gmail.com>
2021-11-13 21:25:58 +0900

    デザインの修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,7 @@
     @yield('content')
     </div>
 
-    <script src="http://studyaid.work/js/app.js"></script>
+    <script src="https://studyaid.work/js/app.js"></script>
     <!-- JQuery -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Bootstrap tooltips -->
@@ -32,7 +32,7 @@
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
     <!-- original JavaScript -->
-    <script src="http://studyaid.work/js/main.js"></script>
+    <script src="https://studyaid.work/js/main.js"></script>
 </body>
 
 </html>
