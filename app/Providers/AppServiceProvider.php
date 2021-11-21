3a6718c691e7cb9fdcc61c9b8f52c91d7f2190ed
notosan-as <yuumi.umi56@gmail.com>
2021-11-21 23:26:01 +0900

    SSL化対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (in_array(config('app.env'), ['prd', 'stg'], true)) {
+        if (in_array(config('env'), ['prd', 'stg'], true)) {
             $url->forceScheme('https');
         }
     }
